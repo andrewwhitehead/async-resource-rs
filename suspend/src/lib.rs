03466657926291d95a181b08f86eaac44844538a
Andrew Whitehead <cywolf@gmail.com>
2020-07-25 11:25:12 -0700

    add Task, Iter, various updates
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- suspend/src/lib.rs
+++ suspend/src/lib.rs
@@ -1,4 +1,5 @@
 use std::cell::UnsafeCell;
+use std::fmt::{self, Display, Formatter};
 use std::future::Future;
 use std::mem::MaybeUninit;
 use std::pin::Pin;
@@ -11,8 +12,11 @@ use std::thread;
 use std::time::{Duration, Instant};
 
 use futures_util::{
+    future::BoxFuture,
     pin_mut,
+    stream::{BoxStream, Stream},
     task::{waker, ArcWake},
+    FutureExt, StreamExt,
 };
 
 // FIXME add Debug impl
@@ -20,6 +24,14 @@ use futures_util::{
 const IDLE: u8 = 0x0;
 const BUSY: u8 = 0x1;
 const LISTEN: u8 = 0x2;
+
+pub fn notify_once<'a>() -> (Notifier, Task<'a, ()>) {
+    let inner = Arc::new(Inner::new(IDLE));
+    let notifier = Notifier {
+        inner: inner.clone(),
+    };
+    (notifier, Task::from_poll(move |cx| inner.poll(cx.waker())))
+}
 
 enum InnerNotify {
     Thread(thread::Thread),
@@ -31,6 +43,17 @@ enum ClearResult {
     NoChange,
     Updated,
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TimeoutError;
+
+impl Display for TimeoutError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Timed out")
+    }
+}
+
+impl std::error::Error for TimeoutError {}
 
 impl ClearResult {
     pub fn is_none(&self) -> bool {
@@ -44,6 +67,13 @@ struct Inner {
 }
 
 impl Inner {
+    pub const fn new(state: u8) -> Self {
+        Self {
+            notify: UnsafeCell::new(MaybeUninit::uninit()),
+            state: AtomicU8::new(state),
+        }
+    }
+
     pub fn acquire<'a>(self: &'a Arc<Self>) -> Option<Listener<'a>> {
         if self.state.compare_and_swap(BUSY, IDLE, Ordering::AcqRel) == BUSY {
             Some(Listener { inner: self })
@@ -71,6 +101,36 @@ impl Inner {
             IDLE => (),
             BUSY => {
                 return false;
+            }
+            LISTEN => {
+                // try to reacquire idle state
+                loop {
+                    match self.state.compare_exchange_weak(
+                        LISTEN,
+                        IDLE,
+                        Ordering::AcqRel,
+                        Ordering::Acquire,
+                    ) {
+                        Ok(_) => {
+                            break unsafe {
+                                self.vacate();
+                            }
+                        }
+                        Err(IDLE) => {
+                            // competition from another thread?
+                            // should not happen with wrappers around this instance
+                            panic!("Invalid state");
+                        }
+                        Err(LISTEN) => {
+                            // retry
+                        }
+                        Err(BUSY) => {
+                            // already notified
+                            return false;
+                        }
+                        Err(_) => panic!("Invalid state"),
+                    }
+                }
             }
             _ => panic!("Invalid state"),
         }
@@ -103,39 +163,6 @@ impl Inner {
             ClearResult::Updated => true,
             ClearResult::NoChange => false,
         }
-    }
-
-    pub fn park(&self) {
-        if !self.listen(InnerNotify::Thread(thread::current())) {
-            // already notified
-            return;
-        }
-        loop {
-            thread::park();
-            if self.state() == BUSY {
-                break;
-            }
-        }
-    }
-
-    pub fn park_timeout(&self, timeout: Duration) -> bool {
-        self.park_until(Instant::now() + timeout)
-    }
-
-    pub fn park_until(&self, expire: Instant) -> bool {
-        if !self.listen(InnerNotify::Thread(thread::current())) {
-            // already notified
-            return false;
-        }
-        while let Some(dur) = expire.checked_duration_since(Instant::now()) {
-            thread::park_timeout(dur);
-            if self.state() == BUSY {
-                // notified
-                return false;
-            }
-        }
-        // timer expired
-        true
     }
 
     pub fn poll(&self, waker: &Waker) -> Poll<()> {
@@ -172,6 +199,39 @@ impl Inner {
     pub unsafe fn vacate(&self) -> InnerNotify {
         self.notify.get().read().assume_init()
     }
+
+    pub fn wait(&self) {
+        if !self.listen(InnerNotify::Thread(thread::current())) {
+            // already notified
+            return;
+        }
+        loop {
+            thread::park();
+            if self.state() == BUSY {
+                break;
+            }
+        }
+    }
+
+    pub fn wait_deadline(&self, expire: Instant) -> bool {
+        if !self.listen(InnerNotify::Thread(thread::current())) {
+            // already notified
+            return true;
+        }
+        while let Some(dur) = expire.checked_duration_since(Instant::now()) {
+            thread::park_timeout(dur);
+            if self.state() == BUSY {
+                // notified
+                return true;
+            }
+        }
+        // timer expired
+        false
+    }
+
+    pub fn wait_timeout(&self, timeout: Duration) -> bool {
+        self.wait_deadline(Instant::now() + timeout)
+    }
 }
 
 impl ArcWake for Inner {
@@ -189,69 +249,69 @@ impl Drop for Inner {
 
 unsafe impl Sync for Inner {}
 
-pub struct NotifyOnce {
-    inner: Inner,
-}
+// pub struct NotifyOnce {
+//     inner: Inner,
+// }
 
-impl NotifyOnce {
-    pub const fn new() -> Self {
-        Self {
-            inner: Inner {
-                notify: UnsafeCell::new(MaybeUninit::uninit()),
-                state: AtomicU8::new(IDLE),
-            },
-        }
-    }
+// impl NotifyOnce {
+//     pub const fn new() -> Self {
+//         Self {
+//             inner: Inner {
+//                 notify: UnsafeCell::new(MaybeUninit::uninit()),
+//                 state: AtomicU8::new(IDLE),
+//             },
+//         }
+//     }
 
-    pub fn cancel(&self) -> bool {
-        match self.inner.clear(false) {
-            ClearResult::NoChange => false,
-            _ => true,
-        }
-    }
+//     pub fn cancel(&self) -> bool {
+//         match self.inner.clear(false) {
+//             ClearResult::NoChange => false,
+//             _ => true,
+//         }
+//     }
 
-    pub fn complete(&self) -> bool {
-        self.inner.notify()
-    }
+//     pub fn complete(&self) -> bool {
+//         self.inner.notify()
+//     }
 
-    pub fn is_complete(&self) -> bool {
-        self.inner.state() == BUSY
-    }
+//     pub fn is_complete(&self) -> bool {
+//         self.inner.state() == BUSY
+//     }
 
-    pub fn park(&self) {
-        self.inner.park()
-    }
+//     pub fn poll(&self, waker: &Waker) -> Poll<()> {
+//         self.inner.poll(waker)
+//     }
 
-    pub fn park_timeout(&self, timeout: Duration) -> bool {
-        self.inner.park_timeout(timeout)
-    }
+//     pub fn wait(&self) {
+//         self.inner.wait()
+//     }
 
-    pub fn park_until(&self, expire: Instant) -> bool {
-        self.inner.park_until(expire)
-    }
+//     pub fn wait_deadline(&self, expire: Instant) -> bool {
+//         self.inner.wait_deadline(expire)
+//     }
 
-    pub fn poll(&self, waker: &Waker) -> Poll<()> {
-        self.inner.poll(waker)
-    }
+//     pub fn wait_timeout(&self, timeout: Duration) -> bool {
+//         self.inner.wait_timeout(timeout)
+//     }
 
-    pub fn waker(self: &Arc<Self>) -> Waker {
-        waker(self.clone())
-    }
-}
+//     pub fn waker(self: &Arc<Self>) -> Waker {
+//         waker(self.clone())
+//     }
+// }
 
-impl ArcWake for NotifyOnce {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        arc_self.complete();
-    }
-}
+// impl ArcWake for NotifyOnce {
+//     fn wake_by_ref(arc_self: &Arc<Self>) {
+//         arc_self.complete();
+//     }
+// }
 
-impl Future for NotifyOnce {
-    type Output = ();
+// impl Future for NotifyOnce {
+//     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        self.inner.poll(cx.waker())
-    }
-}
+//     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+//         self.inner.poll(cx.waker())
+//     }
+// }
 
 pub struct Suspend {
     inner: Arc<Inner>,
@@ -260,10 +320,7 @@ pub struct Suspend {
 impl Suspend {
     pub fn new() -> Self {
         Self {
-            inner: Arc::new(Inner {
-                notify: UnsafeCell::new(MaybeUninit::uninit()),
-                state: AtomicU8::new(BUSY),
-            }),
+            inner: Arc::new(Inner::new(BUSY)),
         }
     }
 
@@ -278,7 +335,7 @@ impl Suspend {
             let mut listen = self.listen();
             match fut.as_mut().poll(&mut cx) {
                 Poll::Ready(result) => break result,
-                Poll::Pending => listen.park(),
+                Poll::Pending => listen.wait(),
             }
         }
     }
@@ -311,16 +368,16 @@ pub struct Listener<'a> {
 }
 
 impl Listener<'_> {
-    pub fn park(&mut self) {
-        self.inner.park()
+    pub fn wait(&mut self) {
+        self.inner.wait()
     }
 
-    pub fn park_timeout(&self, timeout: Duration) -> bool {
-        self.inner.park_timeout(timeout)
+    pub fn wait_deadline(&mut self, expire: Instant) -> bool {
+        self.inner.wait_deadline(expire)
     }
 
-    pub fn park_until(&mut self, expire: Instant) -> bool {
-        self.inner.park_until(expire)
+    pub fn wait_timeout(&self, timeout: Duration) -> bool {
+        self.inner.wait_timeout(timeout)
     }
 }
 
@@ -346,6 +403,10 @@ impl Notifier {
     pub fn notify(&self) {
         self.inner.notify();
     }
+
+    pub fn to_waker(self) -> Waker {
+        waker(self.inner)
+    }
 }
 
 impl Clone for Notifier {
@@ -353,5 +414,234 @@ impl Clone for Notifier {
         Self {
             inner: self.inner.clone(),
         }
+    }
+}
+
+pub type PollFn<'a, T> = Box<dyn FnMut(&mut Context) -> Poll<T> + Send + 'a>;
+
+#[must_use = "Task must be awaited"]
+pub enum Task<'t, T> {
+    Future(BoxFuture<'t, T>),
+    Poll(PollFn<'t, T>),
+}
+
+impl<'t, T> Task<'t, T> {
+    pub fn from_poll<F>(f: F) -> Self
+    where
+        F: FnMut(&mut Context) -> Poll<T> + Send + 't,
+    {
+        Self::Poll(Box::new(f))
+    }
+
+    pub fn from_future<F>(f: F) -> Self
+    where
+        F: Future<Output = T> + Send + 't,
+    {
+        Self::Future(f.boxed())
+    }
+
+    pub fn ready(result: T) -> Self
+    where
+        T: Send + 't,
+    {
+        let mut result = Some(result);
+        Self::from_poll(move |_| {
+            if let Some(result) = result.take() {
+                Poll::Ready(result)
+            } else {
+                Poll::Pending
+            }
+        })
+    }
+
+    pub fn map<'m, F, R>(mut self, f: F) -> Task<'m, R>
+    where
+        F: Fn(T) -> R + Send + 'm,
+        T: 'm,
+        't: 'm,
+    {
+        Task::from_poll(move |cx| Pin::new(&mut self).poll(cx).map(&f))
+    }
+
+    fn poll_inner(&mut self, cx: &mut Context) -> Poll<T> {
+        match &mut *self {
+            Self::Future(fut) => fut.as_mut().poll(cx),
+            Self::Poll(poll) => poll(cx),
+        }
+    }
+
+    pub fn wait(&mut self) -> T {
+        self.wait_while(|listen| {
+            listen.wait();
+            true
+        })
+        .unwrap()
+    }
+
+    pub fn wait_deadline(&mut self, expire: Instant) -> Result<T, TimeoutError> {
+        self.wait_while(|listen| listen.wait_deadline(expire))
+    }
+
+    pub fn wait_timeout(&mut self, timeout: Duration) -> Result<T, TimeoutError> {
+        let expire = Instant::now() + timeout;
+        self.wait_while(|listen| listen.wait_deadline(expire))
+    }
+
+    fn wait_while<F>(&mut self, test: F) -> Result<T, TimeoutError>
+    where
+        F: Fn(&mut Listener) -> bool,
+    {
+        let mut sus = Suspend::new();
+        let waker = sus.waker();
+        let mut cx = Context::from_waker(&waker);
+        let mut listen = sus.listen();
+        loop {
+            match self.poll_inner(&mut cx) {
+                Poll::Ready(result) => break Ok(result),
+                Poll::Pending => {
+                    if !test(&mut listen) {
+                        break Err(TimeoutError);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'f, T> From<BoxFuture<'f, T>> for Task<'f, T> {
+    fn from(fut: BoxFuture<'f, T>) -> Self {
+        Self::Future(fut)
+    }
+}
+
+impl<'f, T> From<PollFn<'f, T>> for Task<'f, T> {
+    fn from(poll: PollFn<'f, T>) -> Self {
+        Self::Poll(poll)
+    }
+}
+
+impl<'f, T> Future for Task<'f, T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.poll_inner(cx)
+    }
+}
+
+pub type PollNextFn<'a, T> = Box<dyn FnMut(&mut Context) -> Poll<Option<T>> + Send + 'a>;
+
+enum IterState<'s, T> {
+    Stream(BoxStream<'s, T>),
+    PollNext(PollNextFn<'s, T>),
+}
+
+impl<'s, T> IterState<'s, T> {
+    fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
+        match self {
+            IterState::Stream(stream) => stream.as_mut().poll_next(cx),
+            IterState::PollNext(poll_next) => poll_next(cx),
+        }
+    }
+}
+
+#[must_use = "Iter must be awaited"]
+pub struct Iter<'s, T> {
+    suspend: Option<Suspend>,
+    state: IterState<'s, T>,
+}
+
+impl<'s, T> Iter<'s, T> {
+    pub fn from_stream<S>(s: S) -> Self
+    where
+        S: Stream<Item = T> + Send + 's,
+    {
+        Self {
+            suspend: None,
+            state: IterState::Stream(s.boxed()),
+        }
+    }
+
+    pub fn from_poll_next<F>(f: F) -> Self
+    where
+        F: FnMut(&mut Context) -> Poll<Option<T>> + Send + 's,
+    {
+        Self {
+            suspend: None,
+            state: IterState::PollNext(Box::new(f)),
+        }
+    }
+
+    pub fn map<'m, F, R>(mut self, f: F) -> Iter<'m, R>
+    where
+        F: Fn(T) -> R + Send + 'm,
+        T: 'm,
+        's: 'm,
+    {
+        Iter::from_poll_next(move |cx| Pin::new(&mut self).poll_next(cx).map(|opt| opt.map(&f)))
+    }
+
+    pub fn wait_next(&mut self) -> Option<T> {
+        self.wait_while(|listen| {
+            listen.wait();
+            true
+        })
+        .unwrap()
+    }
+
+    pub fn wait_next_deadline(&mut self, expire: Instant) -> Result<Option<T>, TimeoutError> {
+        self.wait_while(|listen| listen.wait_deadline(expire))
+    }
+
+    pub fn wait_next_timeout(&mut self, timeout: Duration) -> Result<Option<T>, TimeoutError> {
+        let expire = Instant::now() + timeout;
+        self.wait_while(|listen| listen.wait_deadline(expire))
+    }
+
+    fn wait_while<F>(&mut self, test: F) -> Result<Option<T>, TimeoutError>
+    where
+        F: Fn(&mut Listener) -> bool,
+    {
+        if self.suspend.is_none() {
+            self.suspend.replace(Suspend::new());
+        }
+        let sus = self.suspend.as_mut().unwrap();
+        let waker = sus.waker();
+        let mut listener = sus.listen();
+        let mut cx = Context::from_waker(&waker);
+        loop {
+            match self.state.poll_next(&mut cx) {
+                Poll::Ready(ret) => break Ok(ret),
+                Poll::Pending => {
+                    if !test(&mut listener) {
+                        break Err(TimeoutError);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'s, T> From<PollNextFn<'s, T>> for Iter<'s, T> {
+    fn from(poll: PollNextFn<'s, T>) -> Self {
+        Self {
+            suspend: None,
+            state: IterState::PollNext(poll),
+        }
+    }
+}
+
+impl<'s, T> Iterator for Iter<'s, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.wait_next()
+    }
+}
+
+impl<'s, T> Stream for Iter<'s, T> {
+    type Item = T;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.state.poll_next(cx)
     }
 }
--- suspend/tests/cover.rs
+++ suspend/tests/cover.rs
@@ -1,1 +1,68 @@
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread;
+use std::time::Duration;
 
+use futures_util::stream::Stream;
+
+use suspend::*;
+
+fn simple_timer<'a>(duration: Duration) -> Task<'a, ()> {
+    let (notifier, task) = notify_once();
+    thread::spawn(move || {
+        thread::sleep(duration);
+        notifier.notify()
+    });
+    task
+}
+
+#[test]
+fn task_map() {
+    let mut task = Task::from_future(async { 25 }).map(|x| x * 2);
+    assert_eq!(task.wait(), 50);
+}
+
+#[test]
+fn task_wait_timeout() {
+    let mut task = simple_timer(Duration::from_millis(5));
+    assert!(task.wait_timeout(Duration::from_millis(100)).is_ok());
+
+    let mut task = simple_timer(Duration::from_millis(100));
+    assert_eq!(
+        task.wait_timeout(Duration::from_millis(5)),
+        Err(TimeoutError)
+    );
+}
+
+#[test]
+fn iter_poll_next() {
+    let results = vec![1, 2, 3];
+    let mut rclone = results.clone();
+    rclone.reverse();
+    let iter = Iter::from_poll_next(move |_cx| Poll::Ready(rclone.pop()));
+    assert_eq!(iter.collect::<Vec<i32>>(), results);
+}
+
+#[test]
+fn iter_stream() {
+    struct OddStream(i32);
+
+    impl Stream for OddStream {
+        type Item = i32;
+
+        fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+            let val = self.0;
+            self.0 = val + 1;
+            if val % 2 == 0 {
+                // testing that the waker functions as expected
+                cx.waker().clone().wake();
+                Poll::Pending
+            } else {
+                Poll::Ready(Some(val))
+            }
+        }
+    }
+
+    let iter = Iter::from_stream(OddStream(1));
+    assert_eq!(iter.take(3).collect::<Vec<i32>>(), vec![1, 3, 5]);
+}
